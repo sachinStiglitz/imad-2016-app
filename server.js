e7f8da5f9923edc410c660b54d721a549e65ba95
sachinStiglitz <sachinrdas@gmail.com>
2016-09-20 07:10:55 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -91,15 +91,8 @@ app.get('/', function (req, res) {
 });
 
 app.get('/:articleName',function(req,res) {
+    var articlename=req.params.articleName;
     res.send(createTemplate(articles[articleName]));
-});
-
-app.get('/articleName',function(req,res) {
-      res.send(createTemplate(articles[articleName]));
-});
-
-app.get('/articleName',function(req,res) {
-      res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
