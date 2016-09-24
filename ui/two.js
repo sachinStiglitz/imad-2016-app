bf4af7310938a10b1aab1aac72e13216af871248
sachinStiglitz <sachinrdas@gmail.com>
2016-09-24 17:26:02 +0530

    Update two.js

--- ui/two.js
+++ ui/two.js
@@ -11,8 +11,8 @@ submit.onclick = function() {
            var names2 = request.responseText;
            names2 = JSON.parse(names2);
            var list2 ='';
-           for (var i=0 ; i<names2.length; i++) {
-           list2=list2 + '<li>'+ names2[i] + '</li>';
+           for (var i2=0 ; i2<names2.length; i2++) {
+           list2=list2 + '<li>'+ names2[i2] + '</li>';
            }
            var ul2 = document.getElementById("ul2");
            ul2.innerHTML = list2; 
