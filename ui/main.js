108b9bb4d25b0ae037b260195ddb91caeec0c193
sachinStiglitz <sachinrdas@gmail.com>
2016-09-24 16:17:43 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -23,7 +23,6 @@ button.onclick= function() {
 
           request.open('GET',' http://sachinstiglitz.imad.hasura-app.io/counter', true);
           request.send(null);
-              
 };
 
 
@@ -33,13 +32,29 @@ var name = nameInput.value;
 var submit = document.getElementById("submit_btn");
 submit.onclick = function() {
   //make a request to the server and sent a name
-  
-  //capture a list of names and render it as a list.
-  var names = ['name 1','name 2','name 3','name 4']; 
-  var list ='';
-  for (var i=0 ; i<names.length; i++) {
-      list=list + '<li>'+ names[i] + '</li>';
+// make request
+    var request = new XMLHttpRequest();
+    
+    //capture response
+    request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.DONE){
+        //do something
+        if(request.status === 200){
+            //capture a list of names and render it as a list.
+           var names = request.responseText;
+           names = JSON.parse(names);
+           var list ='';
+           for (var i=0 ; i<names.length; i++) {
+           list=list + '<li>'+ names[i] + '</li>';
   }
-  var ul = document.getElementById("namelist");
-  ul.innerHTML = list;
+           var ul = document.getElementById("namelist");
+           ul.innerHTML = list; 
+        }
+    }
+        //don't do anything
+};
+          //create request response page
+        request.open('GET',' http://sachinstiglitz.imad.hasura-app.io/sunmit-name?name='+ name, true);
+        request.send(null);  
+  
 };
