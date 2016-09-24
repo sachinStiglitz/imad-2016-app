5bf098c9aae9e2c291a95e7a3e118e5b55fd3c63
sachinStiglitz <sachinrdas@gmail.com>
2016-09-24 14:55:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,15 +1,27 @@
 //counter
 
 var button = document.getElementById("counter");
-var counter =0;
+
 button.onclick= function() {
     // make request
+    var request = new XMLHttpRequest();
     
     //capture response
+    request.onreadystatechange = function(){
+    if(request.readyState === XMLHttpRequest.DONE){
+        //do something
+        if(request.status === 200){
+              var counter = request.responseText;
+              var span = document.getElementById("count");
+              span.innerHTML = counter.toString();
     
-    //render the output var
-  counter = counter+ 1;
-  var span = document.getElementById("count");
-  span.innerHTML = counter.toString();
-    
+        }
+    }
+        //don't do anything
+    };
+          //create rewuest response page
+
+          request.open('GET http://coco98.imad.hasura-app.io/Launcher 'true');
+          request.send(null);
+          ////9G
 };
