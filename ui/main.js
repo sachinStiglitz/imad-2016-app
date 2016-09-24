f31537a369947aaceec83b55c10cf8c18f9c81b2
sachinStiglitz <sachinrdas@gmail.com>
2016-09-24 08:24:02 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,13 @@ var element = document.getElementById('main-text');
 element.innerHTML = "New Value";
 
 var img = document.getElementById('madi');
+var marginLeft = 0;
+function moveRight()
+{
+    marginleft = marginLeft+10;
+    img.style.marginLeft = marginLeft +'px';
+}
 img.onclick = function() {
+    var interval = setInterval(moveRight,100);
     img.style.marginLeft = '100px';
 };
