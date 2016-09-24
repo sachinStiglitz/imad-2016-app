2b6e58f8d0d404bc6502d2374359a74969e2541c
sachinStiglitz <sachinrdas@gmail.com>
2016-09-24 17:20:22 +0530

    Update two.js

--- ui/two.js
+++ ui/two.js
@@ -1,4 +1,4 @@
-var submit1 = document.getElementById("btnone");
+var submit2 = document.getElementById("btntwo");
 submit.onclick = function() {
   //make a request to the server and sent a name
     var request = new XMLHttpRequest();
@@ -8,21 +8,21 @@ submit.onclick = function() {
         //do something
         if(request.status === 200){
             //capture a list of names and render it as a list.
-           var names = request.responseText;
-           names = JSON.parse(names);
-           var list ='';
-           for (var i=0 ; i<names.length; i++) {
-           list=list + '<li>'+ names[i] + '</li>';
+           var names2 = request.responseText;
+           names2 = JSON.parse(names2);
+           var list2 ='';
+           for (var i=0 ; i<names2.length; i++) {
+           list2=list2 + '<li>'+ names2[i] + '</li>';
            }
-           var ul = document.getElementById("namelist");
-           ul.innerHTML = list; 
+           var ul2 = document.getElementById("ul2");
+           ul2.innerHTML = list2; 
         }
     }
         //don't do anything
     };
           //create request response page
-        var nameInput = document.getElementById("name");
-        var name = nameInput.value;
-        request.open('GET',' http://sachinstiglitz.imad.hasura-app.io/submit-name?name='+ name, true);
+        var nameInput2 = document.getElementById("two");
+        var name2 = nameInput2.value;
+        request.open('GET',' http://sachinstiglitz.imad.hasura-app.io/two?name='+ name2, true);
         request.send(null);  
 };
