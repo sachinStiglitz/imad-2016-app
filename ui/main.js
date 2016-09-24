5e5028813afe868cec6e62291bd98d6a7df87d31
sachinStiglitz <sachinrdas@gmail.com>
2016-09-24 08:20:42 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,6 +6,6 @@ var element = document.getElementById('main-text');
 element.innerHTML = "New Value";
 
 var img = document.getElementById('madi');
-img.onClick = function() {
+img.onclick = function() {
     img.style.marginLeft = '100px';
 };
